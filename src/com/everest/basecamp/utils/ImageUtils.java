xiaomi-sm8450-marble/android_packages_apps_Personalizations

lockscreen: Custom AOD image display [1/2]
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/crdroid/settings/utils/ImageUtils.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2023-2024 the risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class ImageUtils {
+
+    public static String saveImageToInternalStorage(Context context, Uri imgUri, String featurePath, String filePrefix) {
+        try {
+            InputStream inputStream;
+            if (imgUri.toString().startsWith("content://com.google.android.apps.photos.contentprovider")) {
+                List<String> segments = imgUri.getPathSegments();
+                if (segments.size() > 2) {
+                    String mediaUriString = URLDecoder.decode(segments.get(2), StandardCharsets.UTF_8.name());
+                    Uri mediaUri = Uri.parse(mediaUriString);
+                    inputStream = context.getContentResolver().openInputStream(mediaUri);
+                } else {
+                    throw new FileNotFoundException("Failed to parse Google Photos content URI");
+                }
+            } else {
+                inputStream = context.getContentResolver().openInputStream(imgUri);
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+            String imageFileName = filePrefix + "_" + timeStamp + ".png";
+            File directory = new File("/sdcard/risingOS/" + featurePath);
+            if (!directory.exists() && !directory.mkdirs()) {
+                return null;
+            }
+            File[] files = directory.listFiles((dir, name) -> name.startsWith(filePrefix) && name.endsWith(".png"));
+            if (files != null) {
+                for (File file : files) {
+                    file.delete();
+                }
+            }
+            File file = new File(directory, imageFileName);
+            try (FileOutputStream outputStream = new FileOutputStream(file)) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            }
+            return file.getAbsolutePath();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}

ProjectEverest/packages_apps_BaseCamp

src/com/everest/basecamp/utils/ImageUtils.java
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2023-2024 the risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.everest.basecamp.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class ImageUtils {
+
+    public static String saveImageToInternalStorage(Context context, Uri imgUri, String featurePath, String filePrefix) {
+        try {
+            InputStream inputStream;
+            if (imgUri.toString().startsWith("content://com.google.android.apps.photos.contentprovider")) {
+                List<String> segments = imgUri.getPathSegments();
+                if (segments.size() > 2) {
+                    String mediaUriString = URLDecoder.decode(segments.get(2), StandardCharsets.UTF_8.name());
+                    Uri mediaUri = Uri.parse(mediaUriString);
+                    inputStream = context.getContentResolver().openInputStream(mediaUri);
+                } else {
+                    throw new FileNotFoundException("Failed to parse Google Photos content URI");
+                }
+            } else {
+                inputStream = context.getContentResolver().openInputStream(imgUri);
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+            String imageFileName = filePrefix + "_" + timeStamp + ".png";
+            File directory = new File("/sdcard/everestos/" + featurePath);
+            if (!directory.exists() && !directory.mkdirs()) {
+                return null;
+            }
+            File[] files = directory.listFiles((dir, name) -> name.startsWith(filePrefix) && name.endsWith(".png"));
+            if (files != null) {
+                for (File file : files) {
+                    file.delete();
+                }
+            }
+            File file = new File(directory, imageFileName);
+            try (FileOutputStream outputStream = new FileOutputStream(file)) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            }
+            return file.getAbsolutePath();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
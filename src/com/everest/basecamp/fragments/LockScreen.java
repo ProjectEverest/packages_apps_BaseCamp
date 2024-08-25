xiaomi-sm8450-marble/android_packages_apps_Personalizations

lockscreen: Implement User Switcher [2/2]

src/com/crdroid/settings/fragments/LockScreen.java
@@ -49,6 +49,8 @@ import lineageos.providers.LineageSettings;
 
 import com.android.settings.preferences.ui.PreferenceUtils;
 
+import com.android.internal.util.rising.SystemRestartUtils;
+
 @SearchIndexable
 public class LockScreen extends SettingsPreferenceFragment
             implements Preference.OnPreferenceChangeListener  {
@@ -64,12 +66,14 @@ public class LockScreen extends SettingsPreferenceFragment
     private static final String KEY_UDFPS_ANIMATIONS = "udfps_recognizing_animation_preview";
     private static final String KEY_UDFPS_ICONS = "udfps_icon_picker";
     private static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
+    private static final String KEY_KG_USER_SWITCHER= "kg_user_switcher_enabled";
 
     private Preference mUdfpsAnimations;
     private Preference mUdfpsIcons;
     private Preference mRippleEffect;
     private Preference mWeather;
     private Preference mScreenOffUdfps;
+    private Preference mUserSwitcher;
     
     private OmniJawsClient mWeatherClient;
 
@@ -78,6 +82,9 @@ public class LockScreen extends SettingsPreferenceFragment
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.crdroid_settings_lockscreen);
+        
+        mUserSwitcher = (Preference) findPreference(KEY_KG_USER_SWITCHER);
+        mUserSwitcher.setOnPreferenceChangeListener(this);
 
         PreferenceCategory gestCategory = (PreferenceCategory) findPreference(LOCKSCREEN_GESTURES_CATEGORY);
         PreferenceCategory udfpsCategory = (PreferenceCategory) findPreference(LOCKSCREEN_UDFPS_CATEGORY);
@@ -121,6 +128,10 @@ public class LockScreen extends SettingsPreferenceFragment
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mUserSwitcher) {
+            SystemRestartUtils.showSystemUIRestartDialog(getContext());
+            return true;
+        }
         return false;
     }
 

ProjectEverest/packages_apps_BaseCamp

src/com/everest/basecamp/fragments/LockScreen.java
@@ -40,7 +40,7 @@ import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.util.everest.OmniJawsClient;
-
+import com.android.internal.util.everest.SystemRestartUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -63,11 +63,13 @@ public class LockScreen extends SettingsPreferenceFragment
     private static final String KEY_AUTHENTICATION_ERROR = "fp_error_vibrate";
     private static final String KEY_SCREEN_OFF_UDFPS = "screen_off_udfps_enabled";
     private static final String KEY_WEATHER = "lockscreen_weather_enabled";
+    private static final String KEY_KG_USER_SWITCHER= "kg_user_switcher_enabled";
 
     private PreferenceCategory mFingerprintCategory;
     private SecureSettingSwitchPreference mScreenOffUdfps;
     private Preference mWeather;
     private OmniJawsClient mWeatherClient;
+    private Preference mUserSwitcher;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -86,6 +88,8 @@ public class LockScreen extends SettingsPreferenceFragment
         mWeather = (Preference) findPreference(KEY_WEATHER);
         mWeatherClient = new OmniJawsClient(getContext());
         updateWeatherSettings();
+        mUserSwitcher = (Preference) findPreference(KEY_KG_USER_SWITCHER);
+        mUserSwitcher.setOnPreferenceChangeListener(this);
 
         if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
             prefScreen.removePreference(mFingerprintCategory);
@@ -105,6 +109,10 @@ public class LockScreen extends SettingsPreferenceFragment
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
+        if (preference == mUserSwitcher) {
+            SystemRestartUtils.showSystemUIRestartDialog(getContext());
+            return true;
+        }
         return false;
     }
 